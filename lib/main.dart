08a508a9c206e6eacee821d3cb95799660a0215d
Carlos Morán <moran_krlos@hotmail.com>
2019-11-30 10:07:30 -0600

    ejemplo de un stateful widget en Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,44 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Mi primera aplicación"),
       ),
+      body: Stack(
+        children: <Widget>[
+          Counter(),
+        ],
+      ),
     );
   }
 }
 
+class Counter extends StatefulWidget {
+  @override
+  _CounterState createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  int count;
+  @override
+  void initState() {
+    // TODO: implement initState
+    count = 0;
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Text("Haz dado $count taps."),
+          RaisedButton(
+            child: Text("Tap"),
+            onPressed: (){
+              setState(() {
+                count++;
+              });
+            }
+          ),
+        ],
+      ),
+    );
+  }
+}
